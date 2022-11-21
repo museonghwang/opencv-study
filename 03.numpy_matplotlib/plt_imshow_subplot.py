c18afd75c1a2f712fd242c57d1a6b9669c4f1d3d
museonghwang <musung5147@naver.com>
2022-11-21 15:21:52 +0900

    Add numpy, matplotlib code

--- 03.numpy_matplotlib/plt_imshow_subplot.py
+++ 03.numpy_matplotlib/plt_imshow_subplot.py
@@ -6,7 +6,6 @@ img1 = cv2.imread('./img/wonyoung.jpg')
 img2 = cv2.imread('./img/wonyoung2.png')
 img3 = cv2.imread('./img/wonyoung3.jpg')
 
-
 plt.subplot(1,3,1)  # 1행 3열 중에 1번째
 plt.imshow(img1[:,:,(2,1,0)])
 plt.xticks([]); plt.yticks([])
